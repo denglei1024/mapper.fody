d2f58f12fdbdba6637c3edfa611faa4680f79aec
邓磊 <denglei1024@foxmail.com>
2026-01-06 13:04:15 +0800

    chore: add

---
+++ ObjectMapper.Fody/Attributes/MapToAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace ObjectMapper.Fody.Attributes;
+
+/// <summary>
+/// 定义映射到某个类型的属性。支持多个映射目标类型。
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+public class MapToAttribute(Type destinationType) : Attribute
+{
+    /// <summary>
+    /// 目标类型
+    /// </summary>
+    public Type DestinationType { get; } = destinationType;
+}
---
+++ ObjectMapper.Fody/ModuleWeaver.cs
@@ -0,0 +1,92 @@
+﻿using System.Reflection.Emit;
+using Fody;
+using Mono.Cecil;
+
+namespace ObjectMapper.Fody;
+
+public class ModuleWeaver: BaseModuleWeaver
+{
+    public override void Execute()
+    {
+        // 所有添加了MapFrom或MapTo特性的类，生成映射代码
+        var processor = new MappingProcessor(ModuleDefinition);
+        processor.Process();
+    }
+
+    public override IEnumerable<string> GetAssembliesForScanning()
+    {
+        return Enumerable.Empty<string>();
+    }
+}
+
+public class MappingProcessor(ModuleDefinition moduleDefinition)
+{
+    private ModuleDefinition ModuleDefinition { get; } = moduleDefinition;
+
+    public void Process()
+    {
+        // 扫描所有类型，查找标记了MapFrom或MapTo特性的类
+        foreach (var type in ModuleDefinition.Types)
+        {
+            // 获取类的自定义特性
+            var mapFromAttribute = type.CustomAttributes.FirstOrDefault(x => x.AttributeType.Name == "MapFromAttribute");
+            var mapToAttribute = type.CustomAttributes.FirstOrDefault(x => x.AttributeType.Name == "MapToAttribute");
+            if (mapFromAttribute != null)
+            {
+                // 生成源类型到当前类型的映射代码
+                GenerateMappingCode(type, mapFromAttribute, isFrom: true);
+            }
+
+            if (mapToAttribute != null)
+            {
+                // 生成当前类型到目标类型的映射代码
+                GenerateMappingCode(type, mapToAttribute, isFrom: false);
+            }
+        }
+    }
+
+    private void GenerateMappingCode(TypeDefinition type, CustomAttribute mapAttribute, bool isFrom)
+    {
+        // 获取目标类型
+        var targetType = (TypeReference)mapAttribute.ConstructorArguments[0].Value;
+        var methodName = isFrom ? "MapFrom" : "MapTo";
+
+        // 创建映射方法
+        var method = new MethodDefinition(methodName,
+            MethodAttributes.Public | MethodAttributes.Static,
+            type);
+
+        var parameterType = isFrom ? targetType : type;
+        method.Parameters.Add(new ParameterDefinition("source", ParameterAttributes.None, parameterType));
+
+        var ilProcessor = method.Body.GetILProcessor();
+
+        // 创建目标对象实例
+        ilProcessor.Append(ilProcessor.Create(OpCodes.Newobj, type.Methods.First(m => m.IsConstructor && !m.HasParameters)));
+        ilProcessor.Append(ilProcessor.Create(OpCodes.Stloc_0));
+
+        // 遍历属性，生成赋值代码
+        foreach (var property in type.Properties)
+        {
+            var sourceProperty = isFrom
+                ? targetType.Resolve().Properties.FirstOrDefault(p => p.Name == property.Name)
+                : type.Properties.FirstOrDefault(p => p.Name == property.Name);
+
+            if (sourceProperty != null)
+            {
+                // 生成赋值代码
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Ldloc_0));
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Ldarg_0));
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Callvirt, sourceProperty.GetMethod));
+                ilProcessor.Append(ilProcessor.Create(OpCodes.Callvirt, property.SetMethod));
+            }
+        }
+
+        // 返回目标对象
+        ilProcessor.Append(ilProcessor.Create(OpCodes.Ldloc_0));
+        ilProcessor.Append(ilProcessor.Create(OpCodes.Ret));
+
+        // 将方法添加到类型中
+        type.Methods.Add(method);
+    }
+}
